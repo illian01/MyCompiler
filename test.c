091e20fa285200bc30378768abf1502654ca14a3
Hyunjun Lee <zxzxs9182@gmail.com>
2019-11-30 03:33:42 +0900

    update

--- test.c
+++ test.c
@@ -1,6 +1,16 @@
-int a[3];
+int global_var;
+int global_array[3];
 void main() 
 {
-	a[2]=3;
-	print_d(a[2]);
+	int local_var_init = 10;
+	global_var = local_var_init;
+	print_d(global_var);
+	global_var = global_var + 20;
+	global_array[1] = global_var;
+	print_d(global_array[1]);
+	local_var_init = 2+global_array[1]+2;
+	global_array[1] = local_var_init+2;
+	print_d(global_array[1]);
+	global_array[1] = 2+local_var_init+2;
+	print_d(global_array[1]);
 }
