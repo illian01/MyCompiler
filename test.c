78c4c9525ea56e6cc06aa4360c2c77e8d15232f6
Hyunjun Lee <zxzxs9182@gmail.com>
2019-12-15 21:33:40 +0900

    remove conflict in test.c

--- test.c
+++ test.c
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-=======
 int global_var;
 int global_array[3];
->>>>>>> 1ae0fdd164c0847204243e763f07f0262157b57d
 void main() 
 {
    int local_var;
    int local_var_init = 10;
    int local_array[6];
-   
-<<<<<<< HEAD
-   
-   local_array[3] = 8;
-   local_array[4] = 0;
 
-   printf_d(local_array[3] + local_array[4]);
-}
-}
-=======
    local_array[0] = 0;
    local_array[1] = 0;
    local_array[2] = 0;
@@ -31,4 +19,3 @@ void main()
    
    printf_d(local_var_init);
 }
->>>>>>> 1ae0fdd164c0847204243e763f07f0262157b57d
