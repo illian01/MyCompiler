88a4d029ba2e48803d615be198e18f355ce367cc
Hyunjun Lee <zxzxs9182@gmail.com>
2019-12-15 21:25:28 +0900

    change + to - to use ebp

--- test.c
+++ test.c
@@ -1,4 +1,13 @@
-void main()
+void main() 
 {
-	printf("Hello Compiler");
+   int local_var;
+   int local_var_init = 10;
+   int local_array[6];
+   
+   
+   local_array[3] = 8;
+   local_array[4] = 0;
+
+   printf_d(local_array[3] + local_array[4]);
+}
 }
